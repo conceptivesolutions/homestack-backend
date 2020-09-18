ede32609d0c6a24a9e188f6235176281120d1e2b
w.glanzer <w.glanzer@conceptive.io>
2020-09-18 15:32:49 +0200

    Added API for "metrics" module

---
+++ metrics/api/src/main/java/io/conceptive/netplan/metrics/api/IMetricsExecutor.java
@@ -0,0 +1,30 @@
+package io.conceptive.netplan.metrics.api;
+
+import io.conceptive.netplan.core.model.Device;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Executor to provide metrics analysis
+ *
+ * @author w.glanzer, 18.09.2020
+ */
+public interface IMetricsExecutor
+{
+
+  /**
+   * Determines, if this executor is generally available, or unavailable for any reason
+   *
+   * @return true, if available
+   */
+  boolean canExecute();
+
+  /**
+   * Executes this metric component for a given device
+   *
+   * @param pDevice Device to check
+   * @return the result, not null
+   */
+  @NotNull
+  IMetricsResult execute(@NotNull Device pDevice);
+
+}
---
+++ metrics/api/src/main/java/io/conceptive/netplan/metrics/api/IMetricsResult.java
@@ -0,0 +1,51 @@
+package io.conceptive.netplan.metrics.api;
+
+import org.jetbrains.annotations.*;
+
+/**
+ * Specifies the result for a metrics analyzer
+ *
+ * @author w.glanzer, 18.09.2020
+ */
+public interface IMetricsResult
+{
+
+  enum EState
+  {
+    FAILURE,
+    WARNING,
+    SUCCESS,
+    UNKNOWN
+  }
+
+  /**
+   * @return the current state of this metrics
+   */
+  @NotNull
+  EState getState();
+
+  /**
+   * @return description, why the state is offline / unavailable / etc.
+   */
+  @Nullable
+  String getStateDescription();
+
+  /**
+   * @return the command that was executed by this metrics analyzer
+   */
+  @Nullable
+  default String getExecutedCommand()
+  {
+    return null;
+  }
+
+  /**
+   * @return the command result, if a command was executed
+   */
+  @Nullable
+  default String getCommandResult()
+  {
+    return null;
+  }
+
+}
