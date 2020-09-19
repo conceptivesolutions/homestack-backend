dbc32770bb977bb10c85e8f264924f7854bacb06
w.glanzer <w.glanzer@conceptive.io>
2020-09-19 09:54:52 +0200

    Added docker for mongodb

---
+++ base/src/main/java/io/conceptive/netplan/IDBConstants.java
@@ -0,0 +1,16 @@
+package io.conceptive.netplan;
+
+/**
+ * Contains all constants for DB
+ *
+ * @author w.glanzer, 20.09.2020
+ */
+public interface IDBConstants
+{
+
+  /**
+   * Global name of the mongodb database
+   */
+  String DB_NAME = "netplandev";
+
+}
