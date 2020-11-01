5e76f3073e897bbb803f5bc16286e1c8f840c634
w.glanzer <w.glanzer@conceptive.io>
2020-11-01 22:12:37 +0100

    feat: split metric record logs into separate collections

--- base/src/main/java/io/conceptive/netplan/repository/impl/MetricRecordRepositoryImpl.java
+++ base/src/main/java/io/conceptive/netplan/repository/impl/MetricRecordRepositoryImpl.java
@@ -1,14 +1,13 @@
 package io.conceptive.netplan.repository.impl;
 
-import com.google.common.collect.Sets;
-import com.mongodb.BasicDBObject;
 import com.mongodb.client.model.*;
 import io.conceptive.netplan.core.model.MetricRecord;
 import io.conceptive.netplan.repository.IMetricRecordRepository;
 import org.jetbrains.annotations.NotNull;
 
 import javax.enterprise.context.Dependent;
-import java.util.*;
+import java.util.Set;
+import java.util.stream.*;
 
 /**
  * @author w.glanzer, 12.10.2020
@@ -17,24 +16,30 @@ import java.util.*;
 public class MetricRecordRepositoryImpl extends AbstractRepository<MetricRecord> implements IMetricRecordRepository, IMetricRecordRepository.ITokenlessRepository
 {
 
+  private static final String _RECORDCOLLECTION_PREFIX = "records_";
+
   @NotNull
   @Override
   public Set<MetricRecord> findAll(@NotNull String pDeviceID)
   {
-    return Sets.newHashSet(getCollection().aggregate(List.of(
-        Aggregates.match(Filters.eq("deviceID", pDeviceID)), // filter only for device
-        Aggregates.sort(Sorts.descending("recordTime")), // sort all records by time
-        Aggregates.group("type", Accumulators.first("recordID", "$_id")), // group by type and get recentliest record
-        Aggregates.lookup(getCollectionName(), "recordID", "_id", "records"), // join ID on record
-        Aggregates.replaceRoot(new BasicDBObject("$arrayElemAt", new Object[]{"$records", 0})), // replace root
-        Aggregates.project(Projections.exclude("records")) // exclude previously selected records
-    )));
+    return StreamSupport.stream(mongoClient.getDatabase(getUserID())
+                                    .listCollectionNames()
+                                    .spliterator(), false)
+        .filter(pName -> pName.startsWith(_RECORDCOLLECTION_PREFIX))
+        .map(pCollectionName -> mongoClient.getDatabase(getUserID())
+            .getCollection(pCollectionName, getCollectionType())
+            .find(Filters.eq("deviceID", pDeviceID))
+            .sort(Sorts.descending("recordTime"))
+            .first())
+        .collect(Collectors.toSet());
   }
 
   @Override
   public void addMetricRecord(@NotNull String pUserID, @NotNull MetricRecord pMetricRecord)
   {
-    getCollectionForUser(pUserID).insertOne(pMetricRecord);
+    mongoClient.getDatabase(pUserID)
+        .getCollection(_RECORDCOLLECTION_PREFIX + pMetricRecord.type, getCollectionType())
+        .insertOne(pMetricRecord);
   }
 
   @NotNull
