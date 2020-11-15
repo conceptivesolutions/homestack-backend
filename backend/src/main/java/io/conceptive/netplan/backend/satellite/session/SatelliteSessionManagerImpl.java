473294209fbf6527a15b4714bb3c6651405f093b
w.glanzer <w.glanzer@conceptive.io>
2020-11-15 12:38:06 +0100

    feat: added simple satellite authentication workflow, to retrieve inital config

---
+++ backend/src/main/java/io/conceptive/netplan/backend/satellite/session/SatelliteSessionManagerImpl.java
@@ -0,0 +1,30 @@
+package io.conceptive.netplan.backend.satellite.session;
+
+import io.conceptive.netplan.model.websocket.WebsocketEvent;
+import org.jetbrains.annotations.NotNull;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.Session;
+
+/**
+ * @author w.glanzer, 13.11.2020
+ */
+@ApplicationScoped
+class SatelliteSessionManagerImpl implements ISatelliteSessionManager
+{
+
+  @Override
+  public void registerSession(@NotNull Session pSession)
+  {
+    pSession.addMessageHandler(WebsocketEvent.class, new SatelliteMessageHandler(pSession));
+  }
+
+  @Override
+  public void unregisterSession(@NotNull Session pSession)
+  {
+    pSession.getMessageHandlers().stream()
+        .filter(SatelliteMessageHandler.class::isInstance)
+        .forEach(pSession::removeMessageHandler);
+  }
+
+}
