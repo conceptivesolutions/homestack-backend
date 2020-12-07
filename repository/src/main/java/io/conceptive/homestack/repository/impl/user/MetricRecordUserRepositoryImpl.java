ac2d5a3956d8a2d2d325f424caf385b196ffb29e
w.glanzer <w.glanzer@conceptive.io>
2020-12-07 19:43:36 +0100

    fix: metric records not read correctly from database

--- repository/src/main/java/io/conceptive/homestack/repository/impl/user/MetricRecordUserRepositoryImpl.java
+++ repository/src/main/java/io/conceptive/homestack/repository/impl/user/MetricRecordUserRepositoryImpl.java
@@ -1,5 +1,6 @@
 package io.conceptive.homestack.repository.impl.user;
 
+import com.google.common.collect.Sets;
 import com.mongodb.client.model.*;
 import io.conceptive.homestack.model.data.MetricRecordDataModel;
 import io.conceptive.homestack.repository.api.user.IMetricRecordUserRepository;
@@ -7,7 +8,6 @@ import org.jetbrains.annotations.*;
 
 import javax.enterprise.context.Dependent;
 import java.util.*;
-import java.util.stream.*;
 
 /**
  * @author w.glanzer, 12.10.2020
@@ -16,32 +16,24 @@ import java.util.stream.*;
 class MetricRecordUserRepositoryImpl extends AbstractUserRepository<MetricRecordDataModel> implements IMetricRecordUserRepository
 {
 
-  private static final String _RECORDCOLLECTION_PREFIX = "records_";
-
   @NotNull
   @Override
   public Set<MetricRecordDataModel> findAll(@NotNull String pDeviceID)
   {
-    return StreamSupport.stream(mongoClient.getDatabase(getUserID())
-                                    .listCollectionNames()
-                                    .spliterator(), false)
-        .filter(pName -> pName.startsWith(_RECORDCOLLECTION_PREFIX))
-        .map(pCollectionName -> mongoClient.getDatabase(getUserID())
-            .getCollection(pCollectionName, getCollectionType())
-            .find(Filters.eq("deviceID", pDeviceID))
-            .sort(Sorts.descending("recordTime"))
-            .first())
-        .filter(Objects::nonNull)
-        .collect(Collectors.toSet());
+    return Sets.newHashSet(getCollection().aggregate(List.of(
+        Aggregates.match(Filters.eq("deviceID", pDeviceID)),
+        Aggregates.sort(Sorts.descending("recordTime")),
+        Aggregates.group("$type", Accumulators.first("doc", "$$ROOT")),
+        Aggregates.replaceRoot("$doc")
+    )));
   }
 
   @Nullable
   @Override
   public MetricRecordDataModel findByType(@NotNull String pDeviceID, @NotNull String pType)
   {
-    return mongoClient.getDatabase(getUserID())
-        .getCollection(_RECORDCOLLECTION_PREFIX + pType, MetricRecordDataModel.class)
-        .find(Filters.eq("deviceID", pDeviceID))
+    return getCollection()
+        .find(Filters.and(Filters.eq("deviceID", pDeviceID), Filters.eq("type", pType)))
         .sort(Sorts.descending("recordTime"))
         .first();
   }
